a81f98463591f06fb25f122f090079d070f67580
devalltect00 <rizkyslenovo29@gmail.com>
2023-05-18 22:46:20 +0700

    change settings

--- testdeploy/settings.py
+++ testdeploy/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--q!q57ey!sxq)^a1-@48kz1uvpy=-%0c41nux6po@ys=*z8t=!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
